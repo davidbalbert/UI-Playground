ad15aace2171b678a2dc47c87cc0973dc2b967a7
David Albert <davidbalbert@gmail.com>
2022-08-15 21:19:27 +0100

    Initial Commit

---
+++ UI Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI Playground
+//
+//  Created by David Albert on 8/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI Playground/UI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_PlaygroundApp.swift
+//  UI Playground
+//
+//  Created by David Albert on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct UI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
